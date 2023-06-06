a5b64c1c3dd3a9ab24fd8537d3a3f6008a081dd2
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-06-06 16:43:54 +0200

    Microservices Patient and Notes OK

--- src/main/java/com/ocr/mediscreen_assess/controller/PatientController.java
+++ src/main/java/com/ocr/mediscreen_assess/controller/PatientController.java
@@ -1,21 +1,22 @@
 package com.ocr.mediscreen_assess.controller;
 
 import com.ocr.mediscreen_assess.model.Patient;
+import com.ocr.mediscreen_assess.model.PatientHistory;
 import com.ocr.mediscreen_assess.proxies.MicroservicePatientProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.ocr.mediscreen_assess.service.PatientService;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
 public class PatientController {
-
-//    @Autowired
-//    private PatientService patientService;
 
     private final MicroservicePatientProxy microservicePatientProxy;
 
@@ -27,7 +28,29 @@ public class PatientController {
     public List<Patient> getPatientList() {
         List<Patient> patientList = microservicePatientProxy.patientList();
         return patientList;
-
     }
 
+   @GetMapping(value = "Patient/{lastname}")
+   Optional<Patient> getPatientByLastname(@Valid @PathVariable("lastname") String lastname){
+       Optional<Patient> patient = microservicePatientProxy.getPatientByLastname(lastname);
+        return patient;
+    }
+
+    @PostMapping(value = "/Patient/add")
+    public ResponseEntity<Object> addPatient(@RequestBody Patient patientHistory) {
+        ResponseEntity<Object> patientAdded = microservicePatientProxy.addPatient(patientHistory);
+        return patientAdded;
+    }
+
+    @PutMapping(value = "/Patient/update/{lastname}")
+    Patient updatePatient(@PathVariable String lastname, @RequestBody Patient patientToUpdate){
+        Patient patient= microservicePatientProxy.updatePatient(lastname, patientToUpdate);
+        return patient;
+    }
+
+    @DeleteMapping(value= "/Patient/delete/{lastname}")
+    Patient deletePatient(@PathVariable String lastname){
+        Patient patient = microservicePatientProxy.deletePatient(lastname);
+        return patient;
+    }
 }
--- src/main/java/com/ocr/mediscreen_assess/proxies/MicroservicePatientProxy.java
+++ src/main/java/com/ocr/mediscreen_assess/proxies/MicroservicePatientProxy.java
@@ -2,21 +2,27 @@ package com.ocr.mediscreen_assess.proxies;
 
 import com.ocr.mediscreen_assess.model.Patient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
-    @FeignClient(name = "mediscreen", url = "localhost:8081")
+@FeignClient(name = "mediscreen", url = "localhost:8081")
     public interface MicroservicePatientProxy {
         @GetMapping(value = "/Patients")
         List<Patient> patientList();
 
-        @GetMapping(value = "/Patients/{lastname}")
-        Patient getPatientByLastname(@PathVariable("lastname") String lastname);
+        @GetMapping(value = "/Patient/{lastname}")
+        Optional<Patient> getPatientByLastname(@Valid @PathVariable("lastname") String lastname);
 
+        @PostMapping(value = "/Patient/add")
+        ResponseEntity<Object> addPatient(@Valid @RequestBody Patient patient);
 
+        @PutMapping(value = "/Patient/update/{lastname}")
+        Patient updatePatient(@PathVariable String lastname, @RequestBody Patient patientToUpdate);
 
-
-
+        @DeleteMapping(value="/Patient/delete/{lastname}")
+        Patient deletePatient(@PathVariable String lastname);
     }
