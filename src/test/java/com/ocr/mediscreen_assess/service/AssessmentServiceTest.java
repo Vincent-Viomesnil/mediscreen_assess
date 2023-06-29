f066d3e3148b23ad34db9b259ca85e88773553ca
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-06-30 00:24:58 +0200

    refacto - Avant Docker

--- src/test/java/com/ocr/mediscreen_assess/service/AssessmentServiceTest.java
+++ src/test/java/com/ocr/mediscreen_assess/service/AssessmentServiceTest.java
@@ -2,9 +2,9 @@ package com.ocr.mediscreen_assess.service;
 
 import com.ocr.mediscreen_assess.model.PatientBean;
 import com.ocr.mediscreen_assess.model.PatientHistoryBean;
+import com.ocr.mediscreen_assess.model.TriggerWords;
 import com.ocr.mediscreen_assess.proxies.MicroserviceNotesProxy;
 import com.ocr.mediscreen_assess.proxies.MicroservicePatientProxy;
-import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -28,6 +28,8 @@ public class AssessmentServiceTest {
     @MockBean
     private MicroservicePatientProxy microservicePatientProxy;
 
+
+    private TriggerWords triggerWords = new TriggerWords();
     @InjectMocks
     private AssessmentService assessmentService;
 
@@ -47,10 +49,37 @@ public class AssessmentServiceTest {
         patient.setBirthdate(LocalDate.of(1980, 1, 1));
         patient.setGender("M");
 
+
+        PatientHistoryBean history1 = new PatientHistoryBean();
+        history1.setNotes("None");
+
+
+        when(microservicePatientProxy.getPatientById(patId)).thenReturn(patient);
+
+        // Act
+        String result = assessmentService.getAssessmentById(patId);
+
+        // Assert
+        assertEquals("First Last (age 43) diabetes assessment is: None", result);
+        verify(microservicePatientProxy, times(1)).getPatientById(patId);
+        verify(microserviceNotesProxy, times(1)).getListNotesByPatId(patId);
+    }
+
+
+    @Test
+    public void testGetAssessmentById_Borderline() {
+        // Arrange
+        Long patId = 1L;
+        PatientBean patient = new PatientBean();
+        patient.setFirstname("First");
+        patient.setLastname("Last");
+        patient.setBirthdate(LocalDate.of(1980, 1, 1));
+        patient.setGender("M");
+
         List<PatientHistoryBean> patientHistoryList = new ArrayList<>();
         PatientHistoryBean history1 = new PatientHistoryBean();
-        history1.setNotes("Some notes");
-        patientHistoryList.add(history1);
+        history1.setNotes("Smoker");
+        history1.setNotes("Height");
 
         when(microservicePatientProxy.getPatientById(patId)).thenReturn(patient);
         when(microserviceNotesProxy.getListNotesByPatId(patId)).thenReturn(patientHistoryList);
@@ -64,49 +93,6 @@ public class AssessmentServiceTest {
         verify(microserviceNotesProxy, times(1)).getListNotesByPatId(patId);
     }
 
-    @Test
-    public void testGetAssessmentById_Borderline() {
-        // Arrange
-        Long patId = 1L;
-        PatientBean patient = new PatientBean();
-        patient.setId(patId);
-        patient.setFirstname("John");
-        patient.setLastname("Doe");
-        patient.setBirthdate(LocalDate.of(1970, 1, 1));
-        patient.setGender("M");
-        patient.setAddress("address");
-        patient.setPhonenumber("0123456789");
-
-        List<PatientHistoryBean> patientHistoryList = new ArrayList<>();
-        PatientHistoryBean history1 = new PatientHistoryBean();
-        history1.set_id(new ObjectId());
-        history1.setNoteId(1L);
-        history1.setPatId(1L);
-        history1.setLastname("Doe");
-        history1.setNotes("Height");
-
-        PatientHistoryBean history2 = new PatientHistoryBean();
-        history2.set_id(new ObjectId());
-        history2.setNoteId(2L);
-        history2.setPatId(1L);
-        history2.setLastname("Doe");
-        history2.setNotes("Fumeur");
-
-        patientHistoryList.add(history1);
-        patientHistoryList.add(history2);
-
-
-        when(microservicePatientProxy.getPatientById(patId)).thenReturn(patient);
-        when(microserviceNotesProxy.getListNotesByPatId(patId)).thenReturn(patientHistoryList);
-
-        // Act
-        String result = assessmentService.getAssessmentById(patId);
-
-        // Assert
-        assertEquals("John Doe (age 53) diabetes assessment is: Borderline", result);
-        verify(microservicePatientProxy, times(1)).getPatientById(patId);
-        verify(microserviceNotesProxy, times(1)).getListNotesByPatId(patId);
-    }
 
     @Test
     public void testGetAssessmentById_Danger() {
@@ -115,15 +101,21 @@ public class AssessmentServiceTest {
         PatientBean patient = new PatientBean();
         patient.setFirstname("John");
         patient.setLastname("Doe");
-        patient.setBirthdate(LocalDate.of(1995, 1, 1));
+        patient.setBirthdate(LocalDate.of(2000, 1, 1));
         patient.setGender("M");
 
-        List<PatientHistoryBean> patientHistoryList = new ArrayList<>();
-        PatientHistoryBean history1 = new PatientHistoryBean();
-        history1.setNotes("Trigger 1");
-        history1.setNotes("Trigger 2");
-        history1.setNotes("Trigger 3");
-        patientHistoryList.add(history1);
+        List<PatientHistoryBean> patientHistoryList = new ArrayList<>() {{
+            add(new PatientHistoryBean() {{
+                setNotes("Smoke");
+            }});
+            add(new PatientHistoryBean() {{
+                setNotes("Abnormal");
+            }});
+            add(new PatientHistoryBean() {{
+                setNotes("Reaction");
+            }});
+        }};
+
 
         when(microservicePatientProxy.getPatientById(patId)).thenReturn(patient);
         when(microserviceNotesProxy.getListNotesByPatId(patId)).thenReturn(patientHistoryList);
@@ -132,7 +124,7 @@ public class AssessmentServiceTest {
         String result = assessmentService.getAssessmentById(patId);
 
         // Assert
-        assertEquals("John Doe (age 28) diabetes assessment is: In Danger", result);
+        assertEquals("John Doe (age 23) diabetes assessment is: In Danger", result);
         verify(microservicePatientProxy, times(1)).getPatientById(patId);
         verify(microserviceNotesProxy, times(1)).getListNotesByPatId(patId);
     }
@@ -144,17 +136,29 @@ public class AssessmentServiceTest {
         PatientBean patient = new PatientBean();
         patient.setFirstname("John");
         patient.setLastname("Doe");
-        patient.setBirthdate(LocalDate.of(1985, 1, 1));
-        patient.setGender("F");
+        patient.setBirthdate(LocalDate.of(2000, 1, 1));
+        patient.setGender("M");
 
-        List<PatientHistoryBean> patientHistoryList = new ArrayList<>();
-        PatientHistoryBean history1 = new PatientHistoryBean();
-        history1.setNotes("Trigger 1");
-        history1.setNotes("Trigger 2");
-        history1.setNotes("Trigger 3");
-        history1.setNotes("Trigger 4");
-        history1.setNotes("Trigger 5");
-        patientHistoryList.add(history1);
+        List<PatientHistoryBean> patientHistoryList = new ArrayList<>() {{
+            add(new PatientHistoryBean() {{
+                setNotes("Smoke");
+            }});
+            add(new PatientHistoryBean() {{
+                setNotes("Abnormal");
+            }});
+            add(new PatientHistoryBean() {{
+                setNotes("Reaction");
+            }});
+            add(new PatientHistoryBean() {{
+                setNotes("Antibodies");
+            }});
+
+            add(new PatientHistoryBean() {{
+                setNotes("Weight");
+            }});
+
+        }};
+
 
         when(microservicePatientProxy.getPatientById(patId)).thenReturn(patient);
         when(microserviceNotesProxy.getListNotesByPatId(patId)).thenReturn(patientHistoryList);
@@ -163,7 +167,7 @@ public class AssessmentServiceTest {
         String result = assessmentService.getAssessmentById(patId);
 
         // Assert
-        assertEquals("John Doe (age 38) diabetes assessment is: Early onset", result);
+        assertEquals("John Doe (age 23) diabetes assessment is: Early onset", result);
         verify(microservicePatientProxy, times(1)).getPatientById(patId);
         verify(microserviceNotesProxy, times(1)).getListNotesByPatId(patId);
     }
